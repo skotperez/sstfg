b838c8235f661a95524e74082a71c31c51816da6
skotperez <voraginebramante@gmail.com>
2015-11-09 11:46:12 +0100

    Initial commit: main plugin file

---
+++ sstfg.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Small Steps To Feel Good
+Description: This plugin adds a SSTFG ticket system to the WordPress site bebooda.org.
+Version: 0.1
+Author: montera34
+Author URI: http://montera34.com
+License: GPLv3
+*/
