3a85921f6654af72c45ddd635e2258dbcbbf39d7
skotperez <voraginebramante@gmail.com>
2015-11-15 17:03:31 +0100

    Extra fields in user profile page in dashboard

--- sstfg.php
+++ sstfg.php
@@ -8,12 +8,18 @@ Author URI: http://montera34.com
 License: GPLv3
 */
 
+// ACTIONS and FILTERS
+
 // Add custom post type
 add_action(  'init', 'sstfg_create_post_type', 0 );
 // Custom Taxonomies
 add_action( 'init', 'sstfg_build_taxonomies', 0 );
 // rewrite flush rules
 register_activation_hook( __FILE__, 'sstfg_rewrite_flush' );
+// Register new user contact Methods: custom profile fields
+add_filter( 'user_contactmethods', 'sstfg_extra_user_profile_fields' );
+
+// end ACTIONS and FILTERS
 
 // register post types
 function sstfg_create_post_type() {
@@ -61,9 +67,71 @@ function sstfg_build_taxonomies() {
 	) );
 }
 
+// rewrite flush rules to init post type and taxonomies
 function sstfg_rewrite_flush() {
 	sstfg_create_post_type();
 	sstfg_build_taxonomies();
 	flush_rewrite_rules();
 }
 
+// sstfg extra fields in user profile
+$extra_fields = array(
+//	array(
+//		'name' => __('Extra personal information', 'sstfg'),
+//		'label' => 'user_mobile',
+//		'type' => 'group'
+//	),
+	array(
+		'name' => __('Mobile phone', 'sstfg'),
+		'label' => 'user_mobile',
+		'type' => 'input'
+	),
+//	array(
+//		'name' => __('Ticket access', 'sstfg'),
+//		'label' => 'user_ticket_access',
+//		'type' => 'group'
+//	),
+	array(
+		'name' => __('Ticket Access mode', 'sstfg'),
+		'label' => 'user_ticket_access_mode',
+		'type' => 'radio',
+		'options' => array(
+			'manual' => __('Manual', 'sstfg'),
+			'automatic' => __('Automatic', 'sstfg')
+		)
+	),
+	array(
+		'name' => __('Ticket access regularity', 'sstfg'),
+		'label' => 'user_ticket_access_regularity',
+		'type' => 'radio',
+		'options' => array(
+			'once' => __('Once a week', 'sstfg'),
+			'twice' => __('Twice a week', 'sstfg')
+		)
+	),
+	array(
+		'name' => __('Ticket order', 'sstfg'),
+		'label' => 'user_ticket_order',
+		'type' => 'radio',
+		'options' => array(
+			'random' => __('Random', 'sstfg'),
+			'sequential' => __('Sequential', 'sstfg')
+		)
+	),
+	array(
+		'name' => __('Send me the tickets to my email address', 'sstfg'),
+		'label' => 'user_ticket_order',
+		'type' => 'checkbox'
+	)
+);
+
+// Register new user contact Methods: custom profile fields
+function sstfg_extra_user_profile_fields( $user ) {
+	global $extra_fields;
+	foreach ( $extra_fields as $ef ) {
+	 	$user_fields_method[$ef['label']] = $ef['name'];
+	}
+	return $user_fields_method;
+
+} // end Register new user contact Methods: custom profile fields
+?>
